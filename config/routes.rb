e03ac2dffa844c7f3647b62a2bae6b9979a8aff8
Berat Ertural <Beertural@gmail.com>
2017-09-13 15:15:03 +0200

    added cipher controller, added encrypted text input

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :langs
 
   root 'application#index'
+
+  get '/cipher/index', to: 'cipher#index', as: 'cipher'
+  post '/cipher/index', to: 'cipher#create', as: 'cipher_create' 
 end
